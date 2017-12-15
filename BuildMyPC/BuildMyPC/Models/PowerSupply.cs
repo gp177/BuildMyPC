95f0009cd0195795ae494be1801df8a9aaf84e10
BH210-13\ipd <email>
2017-12-15 12:11:16 -0500

    -- BuildMyPC Updates

--- BuildMyPC/BuildMyPC/Models/PowerSupply.cs
+++ BuildMyPC/BuildMyPC/Models/PowerSupply.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace BuildMyPC.Models
 {
     public class PowerSupply
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string Name { get; set; }
+        [Required]
         public Brand Brand { get; set; }
         public int BrandId { get; set; }
+        [Required]
         public string Efficiency { get; set; }
         public int Watts { get; set; }
         public bool IsModular { get; set; }
