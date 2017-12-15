9c81e383bb8dbebe6a484fcd24b3601243688c9f
GP-PC\GP <Giorgioplescia1@gmail.com>
2017-12-14 20:29:08 -0500

    -- model fixes

--- BuildMyPC/BuildMyPC/Models/Brand.cs
+++ BuildMyPC/BuildMyPC/Models/Brand.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace BuildMyPC.Models
 {
     public class Brand
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
         public string BrandName { get; set; }
+        [Required]
+        [MaxLength(60)]
         public string Country { get; set; }
     }
 }
